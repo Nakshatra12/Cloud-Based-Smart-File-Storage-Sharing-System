5bad4af29e0d673f989b6f3bc401084b27a33f40
Nakshatra <92808177+Nakshatra12@users.noreply.github.com>
2025-11-25 10:33:41 +0530

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    GCP_BUCKET_NAME = os.getenv("GCP_BUCKET_NAME")
-    FIRESTORE_PROJECT_ID = os.getenv("FIRESTORE_PROJECT_ID")
